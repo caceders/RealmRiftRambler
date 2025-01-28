160de913ff51c514248e48b1fde0b35c46462d66
caceders <andersjohnsgaard@hotmail.com>
2025-01-28 14:40:24 +0100

    feat: Add ability to strafe around a centrer node

--- Tools/Strafer/strafer.gd
+++ Tools/Strafer/strafer.gd
@@ -11,6 +11,8 @@ enum StrafeState{
 enum StrafeCentrumType{
 	ENTITY_POSITION,
 	SET_POSITION,
+	SPAWN_POSITION,
+	OTHER_NODE,
 }
 
 @onready var navigation_agent : NavigationAgent2D = self.get_parent().get_node("NavigationAgent2D")
@@ -18,7 +20,7 @@ enum StrafeCentrumType{
 
 @export var enabled: bool = true
 
-@export var strafe_centrum_type: StrafeCentrumType = StrafeCentrumType.ENTITY_POSITION
+@export var strafe_centrum_type: StrafeCentrumType = StrafeCentrumType.SPAWN_POSITION
 
 @export var min_stand_still_time: float = 1
 @export var max_stand_still_time: float = 5
@@ -26,16 +28,18 @@ enum StrafeCentrumType{
 @export var min_strafe_distance: float = 10
 @export var max_strafe_distance: float = 50
 
+@export var other_node: Node2D
 @export var strafe_weight: Vector2 = Vector2(0, 0)
-@export var strafe_cenrtum_is_spawn_position: bool = true
-@export var strafe_centrum_position: Vector2 = Vector2(0, 0)
+@export var set_strafe_centrum_position: Vector2 = Vector2(0, 0)
+
+var _strafe_centrum_position: Vector2 = Vector2(0, 0)
 
 var _active_state: StrafeState = StrafeState.IDLE
 var _stand_still_timer : Timer
 
 func _ready():
-	if strafe_cenrtum_is_spawn_position:
-		strafe_centrum_position = global_position
+	if strafe_centrum_type == StrafeCentrumType.SPAWN_POSITION:
+		_strafe_centrum_position = global_position
 	_stand_still_timer = Timer.new()
 	_stand_still_timer.one_shot = true
 	add_child(_stand_still_timer)
@@ -43,6 +47,15 @@ func _ready():
 func _process(_delta):
 	if not enabled:
 		return
+	match strafe_centrum_type:
+		StrafeCentrumType.ENTITY_POSITION:
+			_strafe_centrum_position = global_position
+		StrafeCentrumType.SET_POSITION:
+			_strafe_centrum_position = set_strafe_centrum_position
+		StrafeCentrumType.OTHER_NODE:
+			if other_node != null:
+				_strafe_centrum_position = other_node.global_position
+
 	match _active_state:
 		StrafeState.IDLE:
 			if _stand_still_timer.time_left == 0:
@@ -77,30 +90,17 @@ func _enter_state(state: StrafeState):
 			_stand_still_timer.start(randf_range(min_stand_still_time, max_stand_still_time))
 			return
 		StrafeState.WALKING:
-			match strafe_centrum_type:
-				StrafeCentrumType.ENTITY_POSITION:
-					var direction = Vector2(randf_range(-1, 1), randf_range(-1, 1)).normalized()
-					var distance = randf_range(min_strafe_distance, max_strafe_distance)
-					navigation_agent.target_position = global_position + (direction * distance) + strafe_weight
-					var tries = 1
-					while not navigation_agent.is_target_reachable() and tries < MAX_TRIES_FOR_NEW_AREA:
-						navigation_agent.target_position = global_position + (direction * distance) + strafe_weight
-						tries += 1
-					if tries >= MAX_TRIES_FOR_NEW_AREA:
-						_enter_state(StrafeState.IDLE)
+			var tries = 1
+			while true:
+				var direction = Vector2(randf_range(-1, 1), randf_range(-1, 1)).normalized()
+				var distance = randf_range(min_strafe_distance, max_strafe_distance)
+				navigation_agent.target_position = _strafe_centrum_position + (direction * distance) + strafe_weight
+				tries += 1
+				if navigation_agent.is_target_reachable():
 					return
-				StrafeCentrumType.SET_POSITION:
-					var direction = Vector2(randf_range(-1, 1), randf_range(-1, 1)).normalized()
-					var distance = randf_range(min_strafe_distance, max_strafe_distance)
-					navigation_agent.target_position = strafe_centrum_position + (direction * distance) + strafe_weight
-					var tries = 1
-					while not navigation_agent.is_target_reachable() and tries < MAX_TRIES_FOR_NEW_AREA:
-						navigation_agent.target_position = strafe_centrum_position + direction * distance + strafe_weight
-						tries += 1
-					if tries >= MAX_TRIES_FOR_NEW_AREA:
-						_enter_state(StrafeState.IDLE)
+				if tries >= MAX_TRIES_FOR_NEW_AREA:
+					_enter_state(StrafeState.IDLE)
 					return
-			return
 				
 
 func enable():
